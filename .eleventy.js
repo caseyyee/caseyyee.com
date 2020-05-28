5cb1fca660731946d396728061b5d3adcc5438f9
caseyyee <caseyyee.ca@gmail.com>
2020-05-28 13:50:50 -0700

    Adds code syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,20 @@
 const CleanCSS = require("clean-css");
-module.exports = function(eleventyConfig) {
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addFilter("cssmin", function(code) {
+  eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "css"
-    ],
+    templateFormats: ["md", "njk", "html", "css"],
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: false,
     passthroughFileCopy: true,
-    pathPrefix: "/"
+    pathPrefix: "/",
   };
 };

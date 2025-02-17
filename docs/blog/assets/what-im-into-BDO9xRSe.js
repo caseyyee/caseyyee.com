da634d37ac1f8246451d2bb4b96dd8ff006738a4
Casey Yee <caseyyee.ca@gmail.com>
2025-02-17 15:37:53 -0800

    build for public

---
+++ docs/blog/assets/what-im-into-BDO9xRSe.js
@@ -0,0 +1,41 @@
+import{u as t,j as e}from"./index-Dm0CXL3C.js";const h={layout:"minimal",title:"What I'm Into",description:"Tech and tools I've enjoyed working with",date:"2025-02-18",showSidebar:!1};function r(i){const n={a:"a",div:"div",h1:"h1",h2:"h2",header:"header",li:"li",p:"p",ul:"ul",...t(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"what-im-into",children:["What I'm Into",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#what-im-into",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.h2,{id:"web-apps-stack",children:["Web Apps Stack",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#web-apps-stack",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:"If I was to start a new project, This would be my stack of choice:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://reactjs.org/",children:"React"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://vitejs.dev/",children:"Vite"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://nextjs.org/",children:"Next.js"})}),`
+`]}),`
+`,e.jsx(n.p,{children:"Styling & Motion"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://tailwindcss.com/",children:"Tailwind CSS"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://lucide.dev/icons/",children:"Lucide Icons"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://www.framer.com/motion/",children:"Framer Motion"})}),`
+`]}),`
+`,e.jsx(n.p,{children:"Type-safe data handling"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://www.typescriptlang.org/",children:"TypeScript"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://zod.dev/",children:"Zod"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://trpc.io/",children:"tRPC"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://tanstack.com/query",children:"React Query"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://www.prisma.io/",children:"Prisma"})}),`
+`]}),`
+`,e.jsx(n.p,{children:"Forms"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://react-hook-form.com/",children:"React hook form"})}),`
+`]}),`
+`,e.jsx(n.p,{children:"Deployment"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://sentry.io/",children:"Sentry"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://cloudflare.com/",children:"Cloudflare"})}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"3d",children:["3D",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#3d",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://threejs.org/",children:"Three.js"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://docs.pmnd.rs/react-three-fiber/getting-started/introduction",children:"React Three Fiber"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://unity.com/",children:"Unity 3D"})}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"cross-platform-native",children:["Cross-platform Native",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#cross-platform-native",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://expo.dev/",children:"Expo"})}),`
+`]})]})}function d(i={}){const{wrapper:n}={...t(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(r,{...i})}):r(i)}export{d as default,h as frontmatter};
